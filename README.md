3f201b10a87e1aa7d9e3701720fbccdb68ac7dcb
mariiaSychova <mariiasychova3@gmail.com>
2024-10-23 16:44:35 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,11 @@
+### My Parser
+
+my parser for test purpose
+
+![Alt text](image.png)
+
+### Example
+
+```rust
+...
+```
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use my_test_parser::list_parser;
+
+pub fn main() {
+    let parsed_data = list_parser::list("[1,1,2,3,5,8]");
+    println!("{:?}", parsed_data);
+    
+    assert_eq!(
+        list_parser::list("[1,1,2,3,5,8]"), 
+        Ok(vec![1, 1, 2, 3, 5, 8])
+    );
+}
